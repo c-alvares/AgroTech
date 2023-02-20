73a406d58bd0039ab93a2c6c9c49e8aec983b90a
c-alvares <cyrobrandaoalvares@gmail.com>
2023-02-19 23:54:08 -0300

    Criado script sql com prisma

---
+++ backend/prisma/migrations/20230220025309_init/migration.sql
@@ -0,0 +1,43 @@
+-- CreateTable
+CREATE TABLE `Users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `management` BOOLEAN NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Drivers` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `licence` INTEGER NOT NULL,
+    `availability` BOOLEAN NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Fleet` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `type` VARCHAR(191) NOT NULL,
+    `plate` VARCHAR(191) NOT NULL,
+    `availability` BOOLEAN NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Maintenance` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `checkin` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `checkout` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `description` VARCHAR(191) NOT NULL,
+    `vehicle_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Maintenance` ADD CONSTRAINT `Maintenance_vehicle_id_fkey` FOREIGN KEY (`vehicle_id`) REFERENCES `Fleet`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
