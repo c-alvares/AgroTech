ad005f0bccfe5b11deb90266cdc5d506bafe08cc
Cyro Alvares <cyrobrandaoalvares@gmail.com>
2023-02-22 09:21:58 -0300

    Alteração do script e models

--- backend/prisma/migrations/20230220153247_add_fields/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `drivers` MODIFY `licence` VARCHAR(191) NOT NULL;
--- backend/prisma/migrations/20230222121957_init/migration.sql
+++ backend/prisma/migrations/20230222121957_init/migration.sql
@@ -12,9 +12,10 @@ CREATE TABLE `Users` (
 CREATE TABLE `Drivers` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
-    `licence` INTEGER NOT NULL,
+    `licence` VARCHAR(191) NOT NULL,
     `availability` BOOLEAN NOT NULL DEFAULT true,
 
+    UNIQUE INDEX `Drivers_licence_key`(`licence`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -32,7 +33,7 @@ CREATE TABLE `Fleet` (
 CREATE TABLE `Maintenance` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `checkin` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `checkout` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `checkout` DATETIME(3) NULL,
     `description` VARCHAR(191) NOT NULL,
     `vehicle_id` INTEGER NOT NULL,
 
