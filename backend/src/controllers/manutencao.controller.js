e4f6d840f85aa0f28b2b253f3e23073be87c6499
Cyro Alvares <cyrobrandaoalvares@gmail.com>
2023-02-22 10:47:36 -0300

    Criação de manutenção e atualiza disponibilidade

--- backend/src/controllers/manutencao.controller.js
+++ backend/src/controllers/manutencao.controller.js
@@ -3,20 +3,39 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 const create = async (req, res) => {
-    let gear = await prisma.Maintenance.create({
-        data: req.body
-    });
+    let { vehicle_id, description } = req.body;
 
-    res.status(201).json(gear).end();
+    const [service, vehicle] = await prisma.$transaction([
+        prisma.Maintenance.create({
+            data: {
+                description : description,
+                vehicle_id : vehicle_id
+            }
+        }),
+        prisma.Fleet.update({
+            where: {
+                id: vehicle_id
+            },
+            data : {
+                availability: false
+            }
+        })
+    ]);
+
+    console.log(service, vehicle);
+
+    res.status(201).json(service).end();
 }
 
-const read = async (req, res) => {
-    let gear = await prisma.Maintenance.findMany();
 
-    res.status(200).json(gear).end();
+const read = async (req, res) => {
+    let service = await prisma.Maintenance.findMany();
+
+    res.status(200).json(service).end();
 }
 
 module.exports = {
     create,
     read
 }
+
