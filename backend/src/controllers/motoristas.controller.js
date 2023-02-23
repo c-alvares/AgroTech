2f4b0764fb30b4912525ceb426e95140d661d805
Cyro Alvares <cyrobrandaoalvares@gmail.com>
2023-02-23 10:37:57 -0300

    API de motoristas finalizada

--- backend/src/controllers/motoristas.controller.js
+++ backend/src/controllers/motoristas.controller.js
@@ -10,13 +10,70 @@ const create = async (req, res) => {
     res.status(201).json(driver).end();
 }
 
+
 const read = async (req, res) => {
     let drivers = await prisma.Drivers.findMany();
 
     res.status(200).json(drivers).end();
 };
 
+
+const readOne = async (req, res) => {
+    let driver = await prisma.Drivers.findUnique({
+        where: {
+            id: Number(req.params.id) 
+        },
+    });
+    res.status(200).json(driver).end();
+};
+
+const update = async (req, res) => {
+    let { name, licence } = req.body;
+
+    let driver = await prisma.Drivers.update({
+        where: {
+            id: Number(req.params.id) 
+        },
+        data: {
+            name, 
+            licence,
+        }
+    });
+
+    res.status(200).json(driver).end();
+}
+
+
+const updateStatus = async (req, res) => {
+    let { availability } = req.body;
+    let driver = await prisma.Drivers.update({
+        where: {
+            id: Number(req.params.id) 
+        },
+        data: {
+            availability
+        }
+    });
+
+    res.status(200).json(driver).end();
+}
+
+
+const remove = async (req, res) => {
+    let driver = await prisma.Drivers.delete({
+        where: {
+            id: Number(req.params.id)
+        }
+    });
+
+    res.status(200).json(driver).end();
+}
+
 module.exports = {
     create,
-    read
+    read,
+    readOne,
+    update,
+    updateStatus,
+    remove
 }
--- backend/src/routes/motoristas.route.js
+++ backend/src/routes/motoristas.route.js
@@ -6,5 +6,9 @@ const Driver = require('../controllers/motoristas.controller');
 
 router.post('/cadastrarmotorista', Driver.create);
 router.get('/listarmotoristas', Driver.read);
+router.get('/buscarmotorista/:id', Driver.readOne);
+router.put('/atualizardados/:id', Driver.update);
+router.put('/alterardisponibilidade/:id', Driver.updateStatus);
+router.delete('/excluirmotorista/:id', Driver.remove);
 
 module.exports = router;
