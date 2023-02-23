d9c674ab1a72fdc0c0e06be0d7addd058c4fecde
Cyro Alvares <cyrobrandaoalvares@gmail.com>
2023-02-23 11:36:29 -0300

    API manutenção, pendente finalização da manutenção

--- backend/src/controllers/manutencao.controller.js
+++ backend/src/controllers/manutencao.controller.js
@@ -34,8 +34,76 @@ const read = async (req, res) => {
     res.status(200).json(service).end();
 }
 
+
+const readOne = async (req, res) => {
+    let service = await prisma.Maintenance.findUnique({
+        where: {
+            id: Number(req.params.id) 
+        },
+    });
+    res.status(200).json(service).end();
+}
+
+
+const update = async (req, res) => {
+    let { description } = req.body;
+    let service = await prisma.Maintenance.update({
+        where: {
+            id: Number(req.params.id) 
+        },
+        data: {
+            description,
+        }
+    });
+
+    res.status(200).json(service).end();
+}
+
+
+const updateStatus = async (req, res) => {
+    let { vehicle_id } = req.body;
+
+    const [service, vehicle] = await prisma.$transaction([
+        prisma.Maintenance.update({
+            where: {
+                id: Number(req.params.id) 
+            },
+            data: {
+                availability: true,
+                checkout: new Date(),
+            }
+        }),
+        prisma.Fleet.update({
+            where: {
+                id: vehicle_id
+            },
+            data : {
+                availability: true
+            }
+        })
+    ]);
+
+    console.log(service, vehicle);
+
+    res.status(200).json(service).end();
+}
+
+const remove = async (req, res) => {
+    let service = await prisma.Maintenance.delete({
+        where: {
+            id: Number(req.params.id)
+        }
+    });
+
+    res.status(200).json(service).end();
+}
+
 module.exports = {
     create,
-    read
+    read,
+    readOne,
+    update,
+    updateStatus,
+    remove
 }
 
--- backend/src/routes/manutencao.route.js
+++ backend/src/routes/manutencao.route.js
@@ -6,5 +6,9 @@ const Maintenance = require('../controllers/manutencao.controller');
 
 router.post('/cadastrarmanutencao', Maintenance.create);
 router.get('/listarmanutencoes', Maintenance.read);
+router.get('/buscarmanutencao/:id', Maintenance.readOne);
+router.put('/atualizartipomanutencao/:id', Maintenance.update);
+router.put('/finalizarmanutencao/:id', Maintenance.updateStatus);
+router.delete('/excluirmanutencao/:id', Maintenance.remove);
 
 module.exports = router;
