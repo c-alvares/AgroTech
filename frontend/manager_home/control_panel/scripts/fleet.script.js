0cb4c99d332138e19836fa58898a8d2c93fc9723
Cyro Alvares <cyrobrandaoalvares@gmail.com>
2023-03-16 10:18:07 -0300

    Adicionado EP Cadastrar Frota

--- frontend/manager_home/control_panel/scripts/fleet.script.js
+++ frontend/manager_home/control_panel/scripts/fleet.script.js
@@ -1,18 +1,55 @@
-const objBT = {
-    bearerToken: "Bearer " + JSON.parse(localStorage.getItem("user")).token,
+const vehicleRegister = {
+  vehicleInput: document.querySelector("#vehicleInput"),
+  plateInput: document.querySelector("#plateInput"),
+};
+
+const vehicleUpdate = {
+  vehicleIdInput: document.querySelector("#vehicleIdInput"),
+  typeUpInput: document.querySelector("#typeUpInput"),
+};
+
+const InputAvailabilityUpdate = {
+  availableVehicleIdInput: document.querySelector("#availableVehicleIdInput"),
+  availabilityVehicleInput: document.querySelector("#availabilityVehicleInput"),
+};
+
+const inputDeleteAuto = { delAutoIdInput: document.querySelector("#delAutoIdInput") };
+
+
+function registerVehicle() {
+  window.event.preventDefault()
+
+  let send = {
+    type: vehicleRegister.vehicleInput.value,
+    plate: vehicleRegister.plateInput.value,
   };
 
-  const driverRegister = {
-    vehicleInput: document.querySelector("#vehicleInput"),
-    licenceInput: document.querySelector("#licenceInput"),
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: objBT.bearerToken,
+    },
   };
-  
-  const driverUpdate = {
-    vehicleIdInput: document.querySelector("#vehicleIdInput"),
-    typeUpInput: document.querySelector("#typeUpInput"),
-  };
-  
-  const inputAvailabilityUpdate = {
-    availableVehicleIdInput: document.querySelector("#availableVehicleIdInput"),
-    availabilityVehicleInput: document.querySelector("#availabilityVehicleInput"),
-  };
+  options.body = JSON.stringify(send);
+
+  fetch("http://localhost:3000/cadastrarveiculo", options)
+    .then((response) => {
+      if (response.status != 201) {
+        if (response.status == 401) {
+          alert("Sessão expirada. Acesse novamente");
+          window.location.reload();
+        } else {
+          alert("Erro ao cadastrar veículo. Tente novamente!");
+          window.location.reload();
+        }
+      } else {
+        return response.json();
+      }
+    })
+    .then((resp) => {
+      alert("Veículo cadastrado com sucesso!");
+      window.location.reload();
+    })
+    .catch((err) => console.error(err));
+}
