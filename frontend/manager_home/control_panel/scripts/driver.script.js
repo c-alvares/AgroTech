4fe7d9fec6395bacba76a7e512eb92e70620697a
Cyro Alvares <cyrobrandaoalvares@gmail.com>
2023-03-16 09:51:25 -0300

    Implementado EP Delete aos motoristas

--- frontend/manager_home/control_panel/scripts/driver.script.js
+++ frontend/manager_home/control_panel/scripts/driver.script.js
@@ -14,82 +14,89 @@
     availabilityInput: document.querySelector("#availabilityInput"),
   };
 
-  function registerDriver() {
-    window.event.preventDefault()
-  
-    let send = {
-      name: driverRegister.driverInput.value,
-      licence: driverRegister.licenceInput.value,
-    };
-  
-    const options = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: objBT.bearerToken,
-      },
-    };
-    options.body = JSON.stringify(send);
-  
-    fetch("http://localhost:3000/cadastrarmotorista", options)
-      .then((response) => {
-        if (response.status != 201) {
-          if (response.status == 401) {
-            alert("Sessão expirada. Acesse novamente");
-          } else {
-            console.log(response.status);
-            alert("Erro ao cadastrar motorista. Tente novamente!");
-          }
+  const inputDelteDriver = { delDriverIdInput: document.querySelector("#delDriverIdInput") };
+
+
+function registerDriver() {
+  window.event.preventDefault()
+
+  let send = {
+    name: driverRegister.driverInput.value,
+    licence: driverRegister.licenceInput.value,
+  };
+
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: objBT.bearerToken,
+    },
+  };
+  options.body = JSON.stringify(send);
+
+  fetch("http://localhost:3000/cadastrarmotorista", options)
+    .then((response) => {
+      if (response.status != 201) {
+        if (response.status == 401) {
+          alert("Sessão expirada. Acesse novamente");
+          window.location.reload();
         } else {
-          return response.json();
+          alert("Erro ao cadastrar motorista. Tente novamente!");
+          window.location.reload();
         }
-      })
-      .then((resp) => {
-        alert("Motorista cadastrado com sucesso!");
-        window.location.reload();
-      })
-      .catch((err) => console.error(err));
-  }
-  
-  function updateDriver() {
-    window.event.preventDefault()
-  
-    let send = {
-      name: driverUpdate.driverUpInput.value,
-      licence: driverUpdate.licenceUpInput.value,
-    };
-  
-    const options = {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: objBT.bearerToken,
-      },
-    };
-    options.body = JSON.stringify(send);
-  
-    fetch(
-      "http://localhost:3000/atualizardados/" + driverUpdate.driverIdInput.value,
-      options
-    )
-      .then((response) => {
-        if (response.status != 200) {
-          if (response.status == 401) {
-            alert("Sessão expirada. Acesse novamente");
-          } else {
-            console.log(response.status);
-            alert("Erro ao atualizar dados. Tente novamente!");
-          }
+      } else {
+        return response.json();
+      }
+    })
+    .then((resp) => {
+      alert("Motorista cadastrado com sucesso!");
+      window.location.reload();
+    })
+    .catch((err) => console.error(err));
+}
+
+
+function updateDriver() {
+  window.event.preventDefault()
+
+  let send = {
+    name: driverUpdate.driverUpInput.value,
+    licence: driverUpdate.licenceUpInput.value,
+  };
+
+  const options = {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: objBT.bearerToken,
+    },
+  };
+  options.body = JSON.stringify(send);
+
+  fetch(
+    "http://localhost:3000/atualizardados/" + driverUpdate.driverIdInput.value,
+    options
+  )
+    .then((response) => {
+      if (response.status != 200) {
+        if (response.status == 401) {
+          alert("Sessão expirada. Acesse novamente");
+          window.location.reload();
         } else {
-          return response.json();
+          alert("Erro ao atualizar dados. Tente novamente!");
+          window.location.reload();
         }
-      })
-      .then((resp) => {
-        alert("Dados cadastrais atualizados com sucesso!");
-        window.location.reload();
-      })
-      .catch((err) => console.error(err));
-  }
+      } else {
+        return response.json();
+      }
+    })
+    .then((resp) => {
+      alert("Dados cadastrais atualizados com sucesso!");
+      window.location.reload();
+    })
+    .catch((err) => console.error(err));
+}
+
 
 function updateAvailability() {
   window.event.preventDefault()
@@ -108,8 +115,10 @@ function updateAvailability() {
         if (response.status != 200) {
           if (response.status == 401) {
             alert("Sessão expirada. Acesse novamente");
+            window.location.reload();
           } else {
             alert("Erro ao alterar disponibilidade. Tente novamente!");
+            window.location.reload();
           }
         } else {
           return response.json();
@@ -121,4 +130,34 @@ function updateAvailability() {
       })
       .catch((err) => console.error(err));
 }
-    
+
+
+function deleteDriver() {
+  const options = {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: objBT.bearerToken
+    },
+  };
+  
+  fetch('http://localhost:3000/excluirmotorista/' + inputDelteDriver.delDriverIdInput.value, options)
+  .then((response) => {
+    if (response.status != 200) {
+      if (response.status == 401) {
+        alert("Sessão expirada. Acesse novamente");
+        window.location.reload();
+      } else {
+        alert("Erro ao excluir motorista. Tente novamente!");
+        window.location.reload();
+      }
+    } else {
+      return response.json();
+    }
+  })
+  .then((resp) => {
+    alert("Motorista excluído com sucesso!");
+    window.location.reload();
+  })
+  .catch((err) => console.error(err));
+}
