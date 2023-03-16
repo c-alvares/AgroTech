5a9e5fda774503063fbdb5396e765805a57473d7
Cyro Alvares <cyrobrandaoalvares@gmail.com>
2023-03-16 08:35:49 -0300

    Alterado nº do status de token expirado

--- backend/src/middleware/middleware.js
+++ backend/src/middleware/middleware.js
@@ -2,11 +2,11 @@ const jwt = require('jsonwebtoken');
 
 const acessValidator = (req, res, next) => {
     let token = req.headers.authorization;
-
+// console.log(token)
     if(token) {
         token = token.split(' ')[1];
         jwt.verify(token, process.env.KEY, (err, data) => {
-            if(err != null) res.status(404).json(err).end();
+            if(err != null) res.status(401).json(err).end();
             else {
                 // console.log(data)
                 if(data.management === true) {
