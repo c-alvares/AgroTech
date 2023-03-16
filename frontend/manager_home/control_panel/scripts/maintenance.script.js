a9bcd0261b20b315ea2aca3bb6911defe4140acf
Cyro Alvares <cyrobrandaoalvares@gmail.com>
2023-03-16 11:17:22 -0300

    Adicionadas variáveis e estrutura de manutenção

---
+++ frontend/manager_home/control_panel/scripts/maintenance.script.js
@@ -0,0 +1,21 @@
+const maintainRegister = {
+    maintenanceDescriptionInput: document.querySelector("#maintenanceDescriptionInput"),
+    priceInput: document.querySelector("#priceInput"),
+    automobileIDInput: document.querySelector("#automobileIDInput"),
+  };
+  
+  const maintainUpdate = {
+    maintIDInput: document.querySelector("#maintIDInput"),
+    mainDescriptionInput: document.querySelector("#mainDescriptionInput"),
+    costInput: document.querySelector("#costInput"),
+  };
+  
+  const InputFinishMaintain = {
+    MaintainIDInput: document.querySelector("#MaintainIDInput"),
+    autoIDInput: document.querySelector("#autoIDInput"),
+  };
+  
+  const inputDeleteMaintain = { delMaintainIdInput: document.querySelector("#delMaintainIdInput") };
+
+
+  
