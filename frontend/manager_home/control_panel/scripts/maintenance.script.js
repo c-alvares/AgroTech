976adece2b62e540f02f6d915fe9d7cfb7461983
Cyro Alvares <cyrobrandaoalvares@gmail.com>
2023-03-16 11:37:03 -0300

    Bug no EP de cadsatro de Manutenção

--- frontend/manager_home/control_panel/scripts/maintenance.script.js
+++ frontend/manager_home/control_panel/scripts/maintenance.script.js
@@ -4,18 +4,55 @@ const maintainRegister = {
     automobileIDInput: document.querySelector("#automobileIDInput"),
   };
   
-  const maintainUpdate = {
+const maintainUpdate = {
     maintIDInput: document.querySelector("#maintIDInput"),
     mainDescriptionInput: document.querySelector("#mainDescriptionInput"),
     costInput: document.querySelector("#costInput"),
   };
   
-  const InputFinishMaintain = {
+const InputFinishMaintain = {
     MaintainIDInput: document.querySelector("#MaintainIDInput"),
     autoIDInput: document.querySelector("#autoIDInput"),
   };
   
-  const inputDeleteMaintain = { delMaintainIdInput: document.querySelector("#delMaintainIdInput") };
+const inputDeleteMaintain = { delMaintainIdInput: document.querySelector("#delMaintainIdInput") };
 
 
-  
+function registerMaintenance() {
+  window.event.preventDefault()
+
+  let send = {
+    description: maintainRegister.maintenanceDescriptionInput.value,
+    cost: maintainRegister.priceInput.value,
+    vehicle_id: maintainRegister.automobileIDInput.value,
+  };
+console.log(send)
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: objBT.bearerToken,
+    },
+  };
+  options.body = JSON.stringify(send);
+
+  fetch("http://localhost:3000/cadastrarmanutencao", options)
+    .then((response) => {
+      if (response.status != 201) {
+        if (response.status == 401) {
+          alert("Sessão expirada. Acesse novamente");
+          window.location.reload();
+        } else {
+          alert("Erro ao cadastrar manutenção. Tente novamente!");
+          window.location.reload();
+        }
+      } else {
+        return response.json();
+      }
+    })
+    .then((resp) => {
+      alert("Manutenção cadastrada com sucesso!");
+      window.location.reload();
+    })
+    .catch((err) => console.error(err));
+}
