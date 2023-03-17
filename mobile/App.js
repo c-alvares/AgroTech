530d91b07bcdefb7c0481608dba9a9305d44d389
Cyro Alvares <cyrobrandaoalvares@gmail.com>
2023-03-17 09:59:50 -0300

    Estrutura mobile criada com respectivas depências

--- mobile/App.js
+++ mobile/App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import LoginScreen from "./src/screens/LoginScreen";
+import DriverScreen from "./src/screens/DriverScreen";
+import FleetScreen from "./src/screens/FleetScreen";
+import MaintenanceScreen from "./src/screens/MaintenanceScreen";
+import OperationScreen from "./src/screens/OperationScreen";
+
+const Stack = createNativeStackNavigator();
+
+// quando utilizar o simulador, máquina virtual, deve-se substituir o localhost da uri pelo ip da máquina (cmd - ipconfig)
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="OperationScreen">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="DriverScreen" component={DriverScreen} />
+        <Stack.Screen name="FleetScreen" component={FleetScreen} />
+        <Stack.Screen name="MaintenanceScreen" component={MaintenanceScreen} />
+        <Stack.Screen name="OperationScreen" component={OperationScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// Recomendação de utilizar post para o login, criando um endpoint no backend utilizando uma query(Select) com where para comparar os dados enviados via body
