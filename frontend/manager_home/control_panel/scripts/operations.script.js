696c8259a4fe9c8eb3663504e960da7ea79d6140
c-alvares <cyrobrandaoalvares@gmail.com>
2023-03-16 22:23:53 -0300

    objetos declarados

---
+++ frontend/manager_home/control_panel/scripts/operations.script.js
@@ -0,0 +1,39 @@
+const registerOperationObj = {
+    opDescriptionInput: document.querySelector("#opDescriptionInput"),
+    opDriverIDInput: document.querySelector("#opDriverIDInput"),
+    opAutomobileIDInput: document.querySelector("#opAutomobileIDInput"),
+};
+  
+const updateOperationObj = {
+    operationIDInput: document.querySelector("#operationIDInput"),
+    operationDescriptionInput: document.querySelector("#operationDescriptionInput"),
+
+};
+  
+const finishOperationObj = {
+    finishOperationIdInput: document.querySelector("#finishOperationIdInput"),
+    operationDriverIDEndInput: document.querySelector("#operationDriverIDEndInput"),
+    operationVehicleIDEndInput: document.querySelector("#operationVehicleIDEndInput")
+};
+  
+const deleteOperationObj = { 
+    delOperationIdInput: document.querySelector("#delOperationIdInput"),
+    operationDriverIDDelInput: document.querySelector("#operationDriverIDDelInput"),
+    operationVehicleIDDelInput: document.querySelector("#operationVehicleIDDelInput"),
+};
+
+function registerOperation() {
+
+}
+
+function updateOperation() {
+
+}
+
+function finishOperation() {
+
+}
+
+function deleteOperation() {
+
+}
