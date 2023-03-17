ae5247534d6901ca8b45877eaf847189c4fd06b7
Cyro Alvares <cyrobrandaoalvares@gmail.com>
2023-03-17 10:37:20 -0300

    Estilo definido

---
+++ mobile/src/styles/style.js
@@ -0,0 +1,63 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#06c406",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  loginsInput: {
+    width: "80%",
+    height: "50px",
+    padding: "1vh",
+    backgroundColor: "#FFF",
+    margin: "5px",
+  },
+
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#911B04',
+  },
+  headerText: {
+    fontSize: '30px',
+    fontFamily: 'Verdana',
+    color: '#FFF',
+    padding: '10px',
+    fontWeight: 'bold',
+    marginLeft: '50px',
+  },
+  boxOrder: {
+    flexDirection: 'flex-start',
+    padding: '15px',
+    margin: '20px',
+    borderRadius: '10px',
+    backgroundColor: '#FB8D13'
+  },
+  orderData: {
+    fontSize: '20px',
+    fontFamily: 'Verdana',
+    color: '#FFF',
+    padding: '4px'
+  },
+
+
+
+  btn1: {
+    height: "50px",
+    borderRadius: "10px",
+    alignItems: "center",
+    justifyContent: "center",
+    margin: "15px",
+    backgroundColor: "#CB1E04"
+  },
+  textBtn1: {
+    fontWeight: "bold",
+    fontSize: "17px",
+    color: "#FFF",
+  },
+
+
+
+});
